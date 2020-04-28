d57e1287663728e2a088c17ba9109803558855d1
Matheus Wilhelm Siqueira <matheus@pop-os.localdomain>
2020-04-28 15:07:01 -0300

    ADD inteligência artificial

--- truco.py
+++ truco.py
@@ -4,22 +4,39 @@ import itertools
 from carta import Carta
 from jogador import Jogador
 
+MAX = 1000
+MIN = -1000
+
 class Truco:
-    def __init__(self, naipes, valores):
+    def __init__(self, naipes, nome, valores):
         self.naipes = naipes
         self.valores = valores
+        self.valoresAux = dict()
         self.baralho = []
+        self.baralhoAux = dict()
         self.jogador1 = Jogador()
         self.jogador2 = Jogador()
-        self.cartaMesa = Carta("","")
+        self.cartaMesa = Carta("", "", "", "")
+        self.nome = nome
 
     def criaBaralho(self):
         baralho = []
+        baralhoAux = dict()
+        valoresAux = dict()
+        subvalor = 1
+
         for i in self.naipes:
-            for j in self.valores:
-                baralho.append(Carta(i,j))
+            for j in range(10):
+                carta = Carta(i,self.nome[j], self.valores[j], subvalor)
+                baralhoAux[carta.getFull()] = carta
+                valoresAux[carta.nome] = carta.valor
+
+                baralho.append(carta)
+            subvalor = subvalor + 1
 
         self.baralho = baralho
+        self.baralhoAux = baralhoAux
+        self.valoresAux = valoresAux
 
     def printaBaralho(self):
         for i in self.baralho:
@@ -29,57 +46,84 @@ class Truco:
         random.shuffle(self.baralho)
 
     def distribuiCartas(self):
+        self.cartaMesa = self.baralho[0]
+        self.baralho.remove(self.baralho[0])
+
+        for k in range(len(self.baralho)):
+            if self.baralho[k].valor == 1 and self.cartaMesa.valor == 10:
+                self.baralho[k].valor = 11
+            else:
+                if self.baralho[k].valor == self.cartaMesa.valor + 1:
+                    self.baralho[k].valor = 11
         for i in range(6):
             if (i % 2 == 0):
                 self.jogador1.mao.append(self.baralho[i])
             else:
                 self.jogador2.mao.append(self.baralho[i])
-        self.cartaMesa = Carta(self.baralho[6].naipe,self.baralho[6].valor)
 
-    def printaMao(self):
+    def printaMao(self, jogador):
+        print("Manilha: " + self.cartaMesa.nome + " " + self.cartaMesa.naipe)
         print("_____________________")
-        for i in range(len(self.jogador1.mao)):
-            print(self.jogador1.mao[i].valor + " " + self.jogador1.mao[i].naipe + "\n")
-        print("_____________________")
-        for i in range(len(self.jogador2.mao)):
-            print(self.jogador2.mao[i].valor + " " + self.jogador2.mao[i].naipe + "\n")
+        for i in range(len(jogador.mao)):
+            #print(jogador.mao[i].nome + " " + jogador.mao[i].naipe + " " + str(jogador.mao[i].valor) + " " +str(jogador.mao[i].subvalor) + "\n")
+            jogador.mao[i].printa()
 
     def limpaMao(self):
         self.jogador1.mao.clear()
         self.jogador2.mao.clear()
 
     def fimDeJogo(self):
-        if(self.jogador1.pontos >= 12 or self.jogador2.pontos >= 12):
+        if(self.jogador1.pontos >= 2 or self.jogador2.pontos >= 2):
+            print("ACABOU!")
             return False
         else:
             return True
 
-    def rodada(self):
-        countP1 = 0
-        countP2 = 0
-        pontosDaRodada = 1
-        self.criaBaralho()
-        self.limpaMao()
-        self.distribuiCartas()
-
-        while (countP1 == 2 or countP2 == 2):
-
-            if self.jogador1.prioridade:
-                self.jogada(self.jogador1)
-                self.jogada(self.jogador2)
-            else:
-                self.jogada(self.jogador2)
-                self.jogada(self.jogador1)
-
-        if countP1 == 2:
-            self.jogador1.pontos += pontosDaRodada
-        if countP2 == 2:
-            self.jogador2.pontos += pontosDaRodada
-        self.baralho.clear()
+    def acabou(self, c1, c2):
+        if c1 == 2 or c2 == 2:
+            return True
+        else:
+            return False
 
     def zeraPontos(self):
         self.jogador1.pontos = 0
         self.jogador2.pontos = 0
+
+    def maiorQueMesa(self, carta):
+        if carta.valor > self.cartaMesa.valor:
+            return True
+        else:
+            return False
+
+    def menorQueMesa(self, carta):
+        if carta.valor < self.cartaMesa.valor:
+            return True
+        else:
+            return False
+
+    def cartaMaior(self, carta0, carta1):
+        if(type(carta1) != "list"):
+            return carta1
+        else:
+            maior = carta1[0]
+
+            for i in carta1:
+                if self.maiorQueMesa(self.baralhoAux[i.info]):
+                    maior = i
+
+            return maior
+
+    def cartaMenor(self, carta0, carta1):
+        if(type(carta1) != "list"):
+            return carta1
+        else:
+            menor = carta1[0]
+
+            for i in carta1:
+                if self.menorQueMesa(self.baralhoAux[i.info]):
+                    menor = i
+
+            return menor
 
     def estadoValido(self, estado):
         size = len(estado)
@@ -127,7 +171,7 @@ class Truco:
 
     def montaArvore(self):
         #Cria uma árvore tendo como raíz a carta atual na mesas
-        tree = arvore.Arvore(arvore.MAX, info=self.cartaMesa.getFull())
+        tree = arvore.Arvore(self.cartaMesa.getFull())
         cartas = []
         #Todos os estados válidos para montar a árvore
         estados = self.todosEstados()
@@ -136,28 +180,176 @@ class Truco:
 
         #Cria o primeiro nível da árvore com as cartas da IA
         for i in self.jogador2.mao:
-            tree.criaAdjacenciaAtual(i.getFull(), 0, arvore.MIN)
+            tree.criaAdjacenciaAtual(i.getFull())
 
         #Cria os níveis na árvore em profundidade
         for i in estados:
             size = len(i) - 1
+            index = tree.getIndex(i[0])
+            node = tree.adjacencias[index]
+
             for j in range(size):
-                tree.criaAdjacencia(i[j], i[j + 1], 0, nivel)
-                #Troca o tipo do nível a cada nó criado
                 nivel = arvore.MIN if nivel == arvore.MAX else arvore.MAX
+
+                node.criaAdjacenciaAtual(i[j + 1])
+
+                #Troca o tipo do nível a cada nó criado
+                index = node.getIndex(i[j + 1])
+                node = node.adjacencias[index]
+            node = tree
 
         return tree
 
-    def jogadaIa(self):
-        #Criação da árvore para a jogada da IA
-        tree = self.montaArvore()
+    def alfabeta(self, profundidade, jogadorMax, estado, alfa, beta):
+        #Condição de parada, se a recursão chegar em uma folha da árvore
+        if profundidade == 0:
+            return estado
+
+        if jogadorMax:
+            valorMax = MIN
+
+            for i in estado.adjacencias:
+                valor = self.alfabeta(profundidade - 1, not jogadorMax, i, alfa, beta)
+                valorMax = self.cartaMaior(valorMax, valor)
+
+            return valorMax
+
+        else:
+            valorMin = MAX
+
+            for i in estado.adjacencias:
+                valor = self.alfabeta(profundidade - 1, not jogadorMax, i, alfa, beta)
+                valorMin = self.cartaMenor(valorMin, valor)
+
+            return valorMin
+
+    def jogadaIa(self, jogador):
+        if len(jogador.mao) > 1:
+            #Criação da árvore para a jogada da IA
+            tree = self.montaArvore()
+            MAX = 1000
+            MIN = -1000
+            melhorOpcao = self.alfabeta(3, True, tree, MIN, MAX)
+            carta = self.baralhoAux[melhorOpcao.info]
+        else:
+            carta = jogador.mao[0]
+
+        jogador.mao.remove(carta)
+        print("Computador jogou: " + carta.getFull())
+        print("--------------------------")
+        return carta
+
+    def jogadaPlayer(self, jogador):
+        self.printaMao(jogador)
+        escolhendo = True
+
+        while escolhendo:
+            try:
+                escolha = int(input("selecione entre 0 e " + str(len(jogador.mao) -1) + ":"))
+                carta = jogador.mao[escolha]
+                escolhendo = False
+            except ValueError:
+                print("Escolha inválida!")
+            except IndexError:
+                print("Escolha fora dos limites!")
+
+        jogador.mao.remove(carta)
+        print("jogador jogou: " + carta.getFull())
+        print("--------------------------")
+        return carta
+
+    def rodada(self):
+        countP1 = 0
+        countP2 = 0
+        rodadas = 1
+        empate = 0
+
+        self.criaBaralho()
+        self.embaralhaBaralho()
+        self.limpaMao()
+        self.distribuiCartas()
+        #INVERTER PARA O JOGADOR 1 COMEÇAR
+        self.jogador1.prioridade = False
+        self.jogador2.prioridade = True
+
+        while (self.acabou(countP1,countP2) == False or empate == 3):
+            #define quem vai jogar
+            if self.jogador1.prioridade:
+                jogada1 = self.jogadaPlayer(self.jogador1)
+                jogada2 = self.jogadaIa(self.jogador2)
+            else:
+                jogada2 = self.jogadaIa(self.jogador2)
+                jogada1 = self.jogadaPlayer(self.jogador1)
+            # valida se a rodada é 2 ou 3 e se os jogadores jogaram cartas iguais e diferentes de manilhas
+            if(jogada1.valor == jogada2.valor and rodadas >= 2 and jogada1.valor != 11):
+                if(self.jogador1.primeiraRodada == True):
+                    countP1 = countP1 + 1
+                    print("============ nova rodada ==============")
+                else:
+                    countP2 = countP2 + 1
+                    print("============ nova rodada ==============")
+            # define se os jogadores jogaram cartas iguais diferentes de manilhas
+            if(jogada1.valor == jogada2 and jogada1.valor == 11):
+                print("entrou no jogadas iguais sem ser manilha")
+                if(jogada1.subvalor > jogada2.subvalor):
+                    countP1 = countP1 + 1
+                    if(rodadas == 1):
+                        self.jogador1.primeiraRodada = True
+
+                    self.jogador1.prioridade = True
+                    self.jogador2.prioridade = False
+                    print("============ nova rodada ==============")
+                else:
+                    countP2 = countP2 + 1
+                    if(rodadas == 1):
+                        self.jogador2.primeiraRodada = True
+                    self.jogador1.prioridade = False
+                    self.jogador2.prioridade = True
+                    print("============ nova rodada ==============")
+            #se empatar na primeira rodada os dois ganham o ponto, se empatar mais vezes ninguem ganha ponto e se empatar 3 vezes sai do loop e ninguem ganha
+            if(jogada1.valor == jogada2.valor):
+                #print("entrou no igual")
+                if(empate == 0):
+                    countP2 = countP2 + 1
+                    countP1 = countP1 + 1
+                    empate = empate + 1
+                    print("============ nova rodada ==============")
+                else:
+                    empate = empate + 1
+            #se uma das cartas for maior que a outra
+            if(jogada1.valor > jogada2.valor):
+                #print("valores : " + str(jogada1.valor) +" " + str(jogada2.valor))
+                countP1 = countP1 + 1
+                #print("entrou no maior")
+                #print(countP1)
+                if(rodadas == 1):
+                    self.jogador1.primeiraRodada = True
+                self.jogador1.prioridade = True
+                self.jogador2.prioridade = False
+                print("============ nova rodada ==============")
+            else:
+                #print("entrou no maior ao contrario")
+                countP2 = countP2 + 1
+                if(rodadas == 1):
+                    self.jogador2.primeiraRodada = True
+                self.jogador1.prioridade = False
+                self.jogador2.prioridade = True
+                print("============ nova rodada ==============")
+
+
+        if countP1 == 2:
+            self.jogador1.pontos += 1
+            print("Player Ganhou")
+        if countP2 == 2:
+            self.jogador2.pontos += 1
+            print("Computador Ganhou")
+        self.baralho.clear()
+        print("==================================")
 
     def jogo(self):
         self.jogador1.prioridade = True
         self.jogador2.prioridade = False
-        self.zeraPontos()
 
         while self.fimDeJogo() == True:
-            self.jogadaIa()
-            self.jogador2.pontos = 13
             self.fimDeJogo()
+            self.rodada()
